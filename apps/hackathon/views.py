d4052389c247a09038db21d013cd9af70c8962f3
Marcus Norona <marcusjeremynorona@gmail.com>
2017-06-29 15:32:12 -0700

    login and registration

--- apps/hackathon/views.py
+++ apps/hackathon/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+import bcrypt
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 def index(request):
+    
+    list = User.objects.all()
+    # for i in list:
+    #     print i.email
     return render(request, 'hackathon/index.html')
+
+
+def register(request):
+
+    if request.method == "POST":
+        values = User.objects.register(request.POST)
+        if values[0]: 
+            request.session['id'] = values[1]
+            return redirect("/home")
+        else:
+            for error in values[1]:
+                messages.error(request, error)
+            return redirect("/")
+    
+
+
+def login(request):
+    if request.method == "POST":
+        login = User.objects.login(request.POST)
+        # print login
+        if login[0]:
+            
+            request.session['id'] = login[2]
+            return redirect('/home')
+        else:
+            messages.error(request, 'Email or password is incorrect')
+            return redirect('/')
+
+def home(request):
+    return render(request, 'hackathon/home.html')
